76c7194e2e8f2d82b7a4b2a1e391100ef9c940ba
Benjamin Nneji <nnejiben@gmail.com>
2022-12-26 14:19:13 +0100

    added more css

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,41 +3,43 @@ import React from "react"
 function Cards() {
     return (
         <section className="cards-section">
+
             <div className="cards card-one">
-                <img src="src/images/katie-zaferes.svg" alt="" />
-                <div className="cards-stats">
-                    <img src="src/images/star.svg" alt="" />
-                    <span>5.0</span>
-                    <span>(6)</span>
-                    <span>• USA</span>
+                <img src="src/images/katie-zaferes.svg" alt="" className="cards--image"/>
+                <div className="cards--stats">
+                    <img src="src/images/star.svg" alt="" className="cards--star" />
+                    <span className="cards--text cards--rating">5.0</span>
+                    <span className="cards--gray">(6)</span>
+                    <span className="cards--gray"> • USA</span>
                 </div>
-                <p>Life lessons with Katie Zaferes</p>
-                <p>From $136 / person</p>
+                <p className="cards--text">Life lessons with Katie Zaferes</p>
+                <p><span className="cards--bold">From $136</span><span className="cards--text"> / person</span></p>
             </div>
 
             <div className="cards card-two">
-                <img src="src/images/wedding-dress.svg" alt="" />
-                <div className="cards-stats">
-                    <img src="src/images/star.svg" alt="" />
-                    <span></span>
-                    <span></span>
-                    <span></span>
+                <img src="src/images/wedding-dress.svg" alt="" className="cards--image"/>
+                <div className="cards--stats">
+                    <img src="src/images/star.svg" alt="" className="cards--star" />
+                    <span className="cards--text cards--rating">5.0</span>
+                    <span className="cards--gray">(6)</span>
+                    <span className="cards--gray"> • USA</span>
                 </div>
-                <p></p>
-                <p></p>
+                <p className="cards--text">Life lessons with Katie Zaferes</p>
+                <p><span className="cards--bold">From $136</span><span className="cards--text"> / person</span></p>
             </div>
 
             <div className="cards card-three">
-                <img src="src/images/mountain-bike1.svg" alt="" />
-                <div className="cards-stats">
-                    <img src="src/images/star.svg" alt="" />
-                    <span></span>
-                    <span></span>
-                    <span></span>
+                <img src="src/images/mountain-bike1.svg" alt="" className="cards--image"/>
+                <div className="cards--stats">
+                    <img src="src/images/star.svg" alt="" className="cards--star" />
+                    <span className="cards--text cards--rating">5.0</span>
+                    <span className="cards--gray">(6)</span>
+                    <span className="cards--gray"> • USA</span>
                 </div>
-                <p></p>
-                <p></p>
+                <p className="cards--text">Life lessons with Katie Zaferes</p>
+                <p><span className="cards--bold">From $136</span><span className="cards--text"> / person</span></p>
             </div>
+
         </section>
     )
 }
