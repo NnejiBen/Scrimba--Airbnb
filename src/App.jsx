3b64fc2d6b886d7c15c2a246aa36ffdf5ffeb095
Benjamin Nneji <nnejiben@gmail.com>
2022-12-27 21:58:51 +0100

    add cardData

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,28 @@ import { useState } from 'react'
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Cards from "./components/Cards"
+import cardData from '../cardData'
 
 function App() {
   const [count, setCount] = useState(0)
+  const cardElements = cardData.map((card) => {
+    return (
+      <Cards 
+        coverImg={card.coverImg}
+        rating={card.stats.rating}
+        reviewCount={card.stats.reviewCount}
+        location={card.location}
+        title={card.title}
+        price={card.price}
+      />
+    )
+  })
 
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <Cards />
+      {cardElements}
     </div>
   )
 }
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,43 +1,20 @@
 import React from "react"
 
-function Cards() {
+function Cards(props) {
     return (
         <section className="cards-section">
 
-            <div className="cards card-one">
-                <img src="src/images/katie-zaferes.svg" alt="" className="cards--image"/>
+            <div className="card">
+                <div className="cards--badge">SOLD OUT</div>
+                <img src={`src/images/${props.coverImg}`} alt="" className="cards--image"/>
                 <div className="cards--stats">
                     <img src="src/images/star.svg" alt="" className="cards--star" />
-                    <span className="cards--text cards--rating">5.0</span>
-                    <span className="cards--gray">(6)</span>
-                    <span className="cards--gray"> • USA</span>
+                    <span className="cards--text cards--rating">{props.rating}</span>
+                    <span className="cards--gray">({props.reviewCount})</span>
+                    <span className="cards--gray"> • {props.location}</span>
                 </div>
-                <p className="cards--text">Life lessons with Katie Zaferes</p>
-                <p><span className="cards--bold">From $136</span><span className="cards--text"> / person</span></p>
-            </div>
-
-            <div className="cards card-two">
-                <img src="src/images/wedding-dress.svg" alt="" className="cards--image"/>
-                <div className="cards--stats">
-                    <img src="src/images/star.svg" alt="" className="cards--star" />
-                    <span className="cards--text cards--rating">5.0</span>
-                    <span className="cards--gray">(6)</span>
-                    <span className="cards--gray"> • USA</span>
-                </div>
-                <p className="cards--text">Life lessons with Katie Zaferes</p>
-                <p><span className="cards--bold">From $136</span><span className="cards--text"> / person</span></p>
-            </div>
-
-            <div className="cards card-three">
-                <img src="src/images/mountain-bike1.svg" alt="" className="cards--image"/>
-                <div className="cards--stats">
-                    <img src="src/images/star.svg" alt="" className="cards--star" />
-                    <span className="cards--text cards--rating">5.0</span>
-                    <span className="cards--gray">(6)</span>
-                    <span className="cards--gray"> • USA</span>
-                </div>
-                <p className="cards--text">Life lessons with Katie Zaferes</p>
-                <p><span className="cards--bold">From $136</span><span className="cards--text"> / person</span></p>
+                <p className="cards--text">{props.title}</p>
+                <p><span className="cards--bold">From ${props.price}</span><span className="cards--text"> / person</span></p>
             </div>
 
         </section>
