5ef15caa9eb78fb7490d3276a850cf6df8726f0e
Benjamin Nneji <nnejiben@gmail.com>
2022-12-28 20:00:08 +0100

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,18 @@ function App() {
     return (
       <Cards 
         key={card.id}
-        coverImg={card.coverImg}
-        rating={card.stats.rating}
-        reviewCount={card.stats.reviewCount}
-        location={card.location}
-        title={card.title}
-        price={card.price}
-        openSpots={card.openSpots}
+        card={card}
+        // Note: The individually listed props below are replaced by card={card}
+        // coverImg={card.coverImg}
+        // rating={card.stats.rating}
+        // reviewCount={card.stats.reviewCount}
+        // location={card.location}
+        // title={card.title}
+        // price={card.price}
+        // openSpots={card.openSpots}
+        /* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax#spread_in_object_literals
+        {...card} can replace card={card} above (see link above for detais).
+        If using this method, remove all "card" after "props" in Card.jsx */
       />
     )
   })
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,23 +2,23 @@ import React from "react"
 
 function Cards(props) {
     let badgeText
-    if (props.openSpots === 0) {
+    if (props.card.openSpots === 0) {
         badgeText = "SOLD OUT"
-    } else if (props.location === "Online") {
+    } else if (props.card.location === "Online") {
         badgeText = "ONLINE"
     }
     return (
         <div className="cards">
             {badgeText && <div className="cards--badge">{badgeText}</div>}
-            <img src={`src/images/${props.coverImg}`} alt="" className="cards--image"/>
+            <img src={`src/images/${props.card.coverImg}`} alt="" className="cards--image"/>
             <div className="cards--stats">
                 <img src="src/images/star.png" alt="" className="cards--star" />
-                <span className="cards--text cards--rating">{props.rating}</span>
-                <span className="cards--gray">({props.reviewCount})</span>
-                <span className="cards--gray"> • {props.location}</span>
+                <span className="cards--text cards--rating">{props.card.stats.rating}</span>
+                <span className="cards--gray">({props.card.stats.reviewCount})</span>
+                <span className="cards--gray"> • {props.card.location}</span>
             </div>
-            <p className="cards--text">{props.title}</p>
-            <p><span className="cards--bold">From ${props.price}</span><span className="cards--text"> / person</span></p>
+            <p className="cards--text">{props.card.title}</p>
+            <p><span className="cards--bold">From ${props.card.price}</span><span className="cards--text"> / person</span></p>
         </div>
     )
 }
